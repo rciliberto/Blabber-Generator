0c097008403a5bc0541ba84860ffc8702343d633
rciliberto <robert.ciliberto@gmail.com>
2020-05-14 20:49:57 -0400

    Add LiamVoice and prepare for GUI view

--- src/main/java/com/rciliberto/blabber/Blabber.java
+++ src/main/java/com/rciliberto/blabber/Blabber.java
@@ -4,14 +4,15 @@ import com.rciliberto.blabber.controller.FeaturesGeneratorController;
 import com.rciliberto.blabber.controller.GeneratorController;
 import com.rciliberto.blabber.model.Generator;
 import com.rciliberto.blabber.model.VoiceGenerator;
-import com.rciliberto.blabber.model.voice.RobVoice;
+import com.rciliberto.blabber.model.voice.Voices;
 import com.rciliberto.blabber.view.GeneratorView;
 import com.rciliberto.blabber.view.PlayerGeneratorView;
 
 public class Blabber {
     public static void main(String[] args) {
-        Generator robVoiceGenerator = new VoiceGenerator(new RobVoice());
-        GeneratorView generatorView = new PlayerGeneratorView(robVoiceGenerator.generate("money printer go brrrrrrrrrrrr"));
+        model = new VoiceGenerator(Voices.ROB_VOICE);
+        view = new PlayerGeneratorView(model.generate("Hello World!"));
+        controller = new FeaturesGeneratorController(model, view);
 
         GeneratorController generatorController = new FeaturesGeneratorController(robVoiceGenerator, generatorView);
         generatorController.start();
